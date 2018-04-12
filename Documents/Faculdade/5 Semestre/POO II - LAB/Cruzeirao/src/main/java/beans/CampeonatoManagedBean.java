305905a87a239ab4522c6a543c90ed626d8fa38d
gilberto.moshim <gilbertoyabikujun@gmail.com>
2018-04-12 13:05:30 -0300

    Lista de juízes

--- Documents/Faculdade/5 Semestre/POO II - LAB/Cruzeirao/src/main/java/beans/CampeonatoManagedBean.java
+++ Documents/Faculdade/5 Semestre/POO II - LAB/Cruzeirao/src/main/java/beans/CampeonatoManagedBean.java
@@ -1,14 +1,21 @@
 package beans;
 
 
+import java.util.ArrayList;
 import java.util.List;
 
 //import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.event.ActionEvent;
 
 import models.Campeonato;
+import models.Categoria;
+import models.Local;
+import models.Tipo;
+import models.Usuario;
 import service.CampeonatoService;
+import service.UsuarioService;
 
 @ManagedBean(eager=true, name = "campeonatoManagedBean")
 @SessionScoped
@@ -16,6 +23,45 @@ public class CampeonatoManagedBean {
 
 	private CampeonatoService service = new CampeonatoService();
 	private Campeonato campeonato = new Campeonato();
+	private ArrayList<Usuario> listaJuizes = new ArrayList<Usuario>(); 
+	public ArrayList<Usuario> getJuizes()
+	{
+		
+		for (Usuario i : UsuarioService.usuarioslist)
+			if (i.getTipo() == Tipo.JUIZ)
+			{
+				listaJuizes.add(i);
+			}
+		return listaJuizes;
+	}
+	
+	public void editarCategoria(ActionEvent event)	
+	{		
+		List<Categoria> categorias = (List<Categoria>)event.getComponent().getAttributes().get("categorias");		
+		campeonato = (Campeonato)event.getComponent().getAttributes().get("campeonato");		
+		if (categorias!=null && categorias.size()>0)	
+		{
+			campeonato.setCategorias(categorias);	
+		}
+		else
+		{
+			campeonato.setCategorias(new ArrayList<Categoria>());		
+		}
+	}
+	
+	public void editarLocal(ActionEvent event)	
+	{		
+		List<Local> locais = (List<Local>)event.getComponent().getAttributes().get("locais");		
+		campeonato = (Campeonato)event.getComponent().getAttributes().get("campeonato");		
+		if (locais!=null && locais.size()>0)	
+		{
+			campeonato.setLocais(locais);	
+		}
+		else
+		{
+			campeonato.setLocais(new ArrayList<Local>());		
+		}
+	}
 	
 	public void salvar()
 	{
