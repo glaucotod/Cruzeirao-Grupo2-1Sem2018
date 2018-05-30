37504262bb081946e542d1d3938f5cfa228e353d
Lucas Chaves <Lucas Chaves@192.168.1.37>
2018-05-29 22:54:21 -0300

    Arrumando !

--- Documents/Faculdade/5 Semestre/POO II - LAB/Cruzeirao/src/main/java/beans/PartidaMB.java
+++ Documents/Faculdade/5 Semestre/POO II - LAB/Cruzeirao/src/main/java/beans/PartidaMB.java
@@ -1,10 +1,12 @@
 package beans;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
+import models.Inscrito;
 import models.Partida;
 import service.PartidaService;
 
@@ -14,10 +16,33 @@ public class PartidaMB {
 
 	private PartidaService service;
 	private Partida partidaSelected;
+	private Inscrito inscritoSelected;
 	
 	public PartidaMB() {
 		this.partidaSelected = new Partida(20);
 		this.service = new PartidaService();
+		this.inscritoSelected = new Inscrito();
+	}
+	
+	
+	
+	public List<Inscrito> getTodosOsInscritosDasDuasEquipes(){
+		
+		List<Inscrito> inscritos = new ArrayList<Inscrito>();
+		
+		if(partidaSelected != null) {
+			if(partidaSelected.getEquipeMandante() != null && partidaSelected.getEquipeMandante().getInscritos() != null)
+				inscritos.addAll(partidaSelected.getEquipeMandante().getInscritos());
+			if(partidaSelected.getEquipeVisitante() != null && partidaSelected.getEquipeVisitante().getInscritos() != null)
+				inscritos.addAll(partidaSelected.getEquipeVisitante().getInscritos());
+		}
+		
+		return inscritos;
+	}
+
+	
+	public void addCartao() {
+		
 	}
 	
 	public List<Partida> getPartidas() {
@@ -30,5 +55,13 @@ public class PartidaMB {
 	
 	public void addPartidaSelected(Partida partida) {
 		this.partidaSelected = partida;
+	}
+	
+	public Inscrito getInscritoSelected() {
+		return inscritoSelected;
+	}
+
+	public void setInscritoSelected(Inscrito inscritoSelected) {
+		this.inscritoSelected = inscritoSelected;
 	}
 }
---
+++ Documents/Faculdade/5 Semestre/POO II - LAB/Cruzeirao/src/main/java/converters/InscritoConverter.java
@@ -0,0 +1,32 @@
+package converters;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+
+import models.Inscrito;
+
+@FacesConverter(value = "InscritoConverter")
+public class InscritoConverter implements Converter {
+	
+		@Override
+	    public Object getAsObject(FacesContext facesContext, UIComponent uiComponent, String value) {
+	        if (value != null && !value.isEmpty()) {
+	            return (Inscrito) uiComponent.getAttributes().get(value);
+	        }
+	        return null;
+	    }
+
+	    @Override
+	    public String getAsString(FacesContext facesContext, UIComponent uiComponent, Object value) {
+	        if (value instanceof Inscrito) {
+	            Inscrito entity= (Inscrito) value;
+	            if (entity != null && entity.getUsuario() != null && entity.getUsuario().getNome() != null) {
+	                uiComponent.getAttributes().put( entity.getUsuario().getNome(), entity);
+	                return entity.getUsuario().getNome();
+	            }
+	        }
+	        return "";
+	    }
+}
